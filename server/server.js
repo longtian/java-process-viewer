d239baaf2b16697d6780844f6374a01b481231fb
yan <wyvernnot@gmail.com>
2017-07-21 15:10:44 +0800

    build

--- server/server.js
+++ server/server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const assert = require('assert');
 const morgan = require('morgan');
+const path = require('path');
 
 assert.ok(process.env.ES, 'ES host must be provided');
 
-const webpackMiddleWare = require('./middleware/webpack');
 const apiRouter = require('./route/api');
 
 const app = express();
 
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleWare = require('./middleware/webpack');
+  app.use(webpackMiddleWare)
+} else {
+  app.use(express.static(path.join(__dirname, 'dist')));
+}
+
+
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
-app.use(webpackMiddleWare)
 
 app.use('/api', apiRouter);
 
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
   },
   module: {
     rules: [
